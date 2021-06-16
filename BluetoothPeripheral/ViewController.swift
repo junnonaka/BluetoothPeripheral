6e6d3de8423442354c520606aa6a81f040ed1476
junnonaka <junnonaka6142@gmail.com>
2021-06-16 22:24:30 +0900

    Initial Commit

---
+++ BluetoothPeripheral/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BluetoothPeripheral
+//
+//  Created by 野中淳 on 2021/06/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
